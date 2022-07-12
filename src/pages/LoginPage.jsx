f32b50387022cd96da8a3f55de3e04f93206908c
grenzk <74033991+grenzk@users.noreply.github.com>
2022-07-12 16:13:33 +0800

    Add background images

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import loginBackground from '../assets/img/website-designing.svg'
+import loginBackgroundToo from '../assets/img/collaborate.svg'
 
 const LoginPage = () => {
   return (
     <div>
-      <h1>Login</h1>
+      {/* <div>
+        <img
+          src={loginBackground}
+          className="login-bg one"
+          alt="login background one"
+        />
+        <img
+          src={loginBackgroundToo}
+          className="login-bg two"
+          alt="login background two"
+        />
+      </div> */}
     </div>
   )
 }
