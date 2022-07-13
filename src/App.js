d1483d34f58412496949621c18f2e9eecbba5ae0
grenzk <74033991+grenzk@users.noreply.github.com>
2022-07-13 15:26:37 +0800

    Add main page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import LandingPage from './pages/LandingPage'
 import LoginPage from './pages/LoginPage'
+import MainPage from './pages/MainPage'
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
+        <Route path="/" element={<MainPage />} />
         <Route path="LoginPage" element={<LoginPage />} />
       </Routes>
     </Router>
