2f955c22ff13e95214e2ec7f6de116ad7604508c
louisordonez <72607877+louisordonez@users.noreply.github.com>
2022-07-15 17:11:28 +0800

    Update LandingPage.jsx

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from '../components/LandingPage/Header'
 import Content from '../components/LandingPage/Content'
 import SecondaryContent from '../components/LandingPage/SecondaryContent'
 import Footer from '../components/LandingPage/Footer'
 
 const LandingPage = () => {
+  const [headerBool, setHeaderBool] = useState(false);
+  
+  const handleHeaderBool = (bool) => {
+   setHeaderBool(bool)
+  }
+  
   return (
     <div>
-      <Header />
+      <Header onHeaderBool={handleHeaderBool} />
       <Content />
       <SecondaryContent />
       <Footer />
