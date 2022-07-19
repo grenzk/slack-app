2258212bc21eb3fab705d167e45163a6c7d5cbb8
grenzk <74033991+grenzk@users.noreply.github.com>
2022-07-19 19:49:38 +0800

    Modify code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import LandingPage from './pages/LandingPage'
 import LoginPage from './pages/LoginPage'
 import MainPage from './pages/MainPage'
@@ -9,14 +9,14 @@ import { UserContextProvider } from './contexts/User'
 const App = () => {
   return (
     <UserContextProvider>
-      <div>
+      <Router>
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="LoginPage" element={<LoginPage />} />
           <Route path="SignUpPage" element={<SignUpPage />} />
           <Route path="MainPage/*" element={<MainPage />} />
         </Routes>
-      </div>
+      </Router>
     </UserContextProvider>
   )
 }
